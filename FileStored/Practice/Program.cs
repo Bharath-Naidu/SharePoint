1668d14da9c9c0fd1ed9b2c1201001324dd7ce33
Bharath-Naidu <nbharat229@gmail.com>
2018-10-14 01:23:46 +0530

    NewUpdates

--- FileStored/Practice/Program.cs
+++ FileStored/Practice/Program.cs
@@ -9,10 +9,13 @@ using System.Threading.Tasks;
 using SP = Microsoft.SharePoint.Client;
 using DocumentFormat.OpenXml.Spreadsheet;
 using DocumentFormat.OpenXml.Packaging;
+using F=System.IO;
+
 namespace Practice
 {
     class Program
     {
+        public static DataTable OriginaldataTable=new DataTable("New");
         static void Main(string[] args)
         {
             String UserName = "bharat.naidu@acuvate.com";
@@ -35,7 +38,10 @@ namespace Practice
                     con.Load(Rfile);
                     con.ExecuteQuery();
                     if (Rfile.Name.Equals("FilesInformation.xlsx"))
+                    {
                         InsertIntoDataTable(con, Rfile.Name);
+                        break;
+                    }
                 }
             }
             Console.ReadKey();
@@ -43,11 +49,11 @@ namespace Practice
         public static void InsertIntoDataTable(ClientContext con,String fileName)
         {
             string strErrorMsg = string.Empty;
-            string lstDocName = "Documents";
+            string RootFile = "Documents";
             try
             {
                 DataTable dataTable = new DataTable("FileInformation");
-                List list = con.Web.Lists.GetByTitle(lstDocName);
+                List list = con.Web.Lists.GetByTitle(RootFile);
                 con.Load(list.RootFolder);
                 con.ExecuteQuery();
                 string fileServerRelativeUrl = list.RootFolder.ServerRelativeUrl + "/" + fileName;
@@ -86,12 +92,14 @@ namespace Practice
                                     dataTable.Rows.Add(dataRow);
                                 }
                             }
-                            dataTable.Rows.RemoveAt(0);
+                             dataTable.Rows.RemoveAt(0);
                         }
                     }
                 }
-                Display(con,dataTable, fileName);
-
+                OriginaldataTable = dataTable.Copy();
+                
+                ReadFile(con,dataTable, fileName);
+                display();
             }
             catch (Exception e)
             {
@@ -99,44 +107,103 @@ namespace Practice
                 strErrorMsg = e.Message;
             }
         }
-       public static void Display(ClientContext con,DataTable data,String fileName)
+        public static void display()
         {
+            foreach (DataRow DR in OriginaldataTable.Rows)
+            {
+                foreach(var item in DR.ItemArray)
+                    Console.WriteLine(item);
+
+            }
+        }
+        public static void UpdateDataTable(String FilePath,String Reason,String Status)
+        {
+            foreach (DataRow DR in OriginaldataTable.Rows)
+            {
+                if(DR[0].Equals(FilePath))
+                {
+                    DR[4] = Status;
+                    DR[5] = Reason;
+                }
+               
+            }
+        }
+        public static void ReadFile(ClientContext con,DataTable data,String fileName)
+        {
+            String Reason = "";
+            String UploadStatus = "";
+            String FilePath = "";
             List list = con.Web.Lists.GetByTitle("Documents");
             con.Load(list);
             con.ExecuteQuery();
             Folder newFolder = list.RootFolder;
-            newFolder.Folders.Add("FileUpload");
             con.Load(newFolder);
             con.ExecuteQuery();
-            //String URL = newFolder.ServerRelativeUrl;
-            Console.WriteLine(fileName);
+            int count = 0;
             foreach (DataRow row in data.Rows)
             {
+                bool flag = true;
+                long bytes = 0;
+               
                 try
                 {
-                    String FilePath = row[0].ToString();
-                    String FileName = row[1].ToString();
-                    Console.WriteLine(FilePath);
-                    Console.WriteLine(FileName);
-                    String url = newFolder.ServerRelativeUrl;
-                    Folder folder = con.Web.GetFolderByServerRelativeUrl(url);
-                    FileCreationInformation fci = new FileCreationInformation();
-                    fci.Content = System.IO.File.ReadAllBytes(FilePath);
-                    fci.Url = FileName;
-                    fci.Overwrite = true;
-                    File fileToUpload = folder.Files.Add(fci);
-                    con.Load(fileToUpload);
-                    con.ExecuteQuery();
-                    Console.WriteLine("Done");
-                    break;
+                    FilePath = row[0].ToString();
+                    System.IO.FileInfo fileInfo = new System.IO.FileInfo(FilePath);
+                    if (fileInfo.Exists)
+                        bytes = fileInfo.Length;
+                    else
+                    {
+                        UploadStatus = "Failed";
+                        Reason = "File not exist on given path";
+                        flag = false;
+                    }
+                    if (flag == true && (bytes < 10000000))
+                    {
+
+                        int last = FilePath.LastIndexOf("\\");
+                        String Filename = FilePath.Substring(last + 1);
+                        String CreatedBy = row[1].ToString();
+                        int depart = Convert.ToInt32(row[2]);
+                        String Status = (row[3]).ToString();
+                        Console.WriteLine(FilePath);
+                        Console.WriteLine(CreatedBy);
+
+                        FileCreationInformation fci = new FileCreationInformation();
+                        fci.Content = System.IO.File.ReadAllBytes(FilePath);
+                        fci.Url = Filename;
+                        fci.Overwrite = true;
+
+                        File fileToUpload = newFolder.Files.Add(fci);
+                        con.Load(fileToUpload);
+                        con.ExecuteQuery();
+
+                        var newItem = fileToUpload.ListItemAllFields;
+                        newItem["CreatedByThisFile"] = CreatedBy.ToString();
+                        newItem["Size"] = fileToUpload.Length;
+                        newItem["Dept"] = depart;
+                        newItem["Status"] = Status.ToString();
+                        newItem.Update();
+                        con.ExecuteQuery();
+                        
+                        Console.WriteLine("Done");
+                        UploadStatus = "Success";
+                        Reason = "None";
+                    }
+                    else if (flag)
+                    {
+                        Reason = "File size is to high";
+                        UploadStatus = "failed";
+                    }
+                    UpdateDataTable(FilePath,Reason,UploadStatus);
                 }
-                catch(Exception ex)
+                catch (Exception ex)
                 {
                     Console.WriteLine(ex);
                     break;
                 }
                 
             }
+                    
         }
         private static string GetCellValue(ClientContext clientContext, SpreadsheetDocument document, Cell cell)
         {
